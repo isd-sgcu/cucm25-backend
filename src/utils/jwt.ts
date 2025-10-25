cb3252eb7af5f9318290fc565ceae56a5bacde9b
chawinkn <chawin26112549@gmail.com>
2025-10-26 01:29:13 +0700

    feat: add jwt
    - create jwt utils
    - update .env with jwt and keycloak base url

---
+++ src/utils/jwt.ts
@@ -0,0 +1,30 @@
+import { User } from "@prisma/client"
+import jwt from "jsonwebtoken"
+
+const JWT_SECRET = process.env.JWT_SECRET || "JWTSECRET"
+const EXPIRES_IN = "3d"
+
+export type jwtUser = Pick<User, "id" | "username" | "role">
+
+export interface jwtKeycloakUser {
+    sub: string
+    groups: Array<string>
+    studentId: string
+    nickName: string
+    name: string
+    preferred_username: string
+    given_name: string
+    family_name: string
+}
+
+export function signJwt(user: jwtUser): string {
+    return jwt.sign(user, JWT_SECRET, { expiresIn: EXPIRES_IN })
+}
+
+export function verifyJwt(token: string): jwtUser {
+    return jwt.verify(token, JWT_SECRET) as jwtUser
+}
+
+export function decodeKeycloakJwt(token: string): jwtKeycloakUser {
+    return jwt.decode(token) as jwtKeycloakUser
+}
