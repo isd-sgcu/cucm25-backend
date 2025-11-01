8ee52453f113af57adf28bf69202acbcc8330163
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-01 21:01:51 +0700

    chore: add ANOTHER note saying that this is just a mock user

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,3 +1,10 @@
+/**
+ * NOTE: Due to the lack of the actual authentication for this project (as of November 1, 2025),
+ * this file is used to create the type of mock users instead to test gifting functionality.
+ * TODO: Integrate the gifting functionality (this) with a proper authentication method once
+ * it gets implemented/merged (maybe delete this file as well idk) -mistertfy64 2025-11-01
+ */
+
 import { EducationLevel, RoleType, User } from "@prisma/client";
 
 export interface ParsedUser {
