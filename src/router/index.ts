ca821f379c8e0d396ada1bfb0722e8e850680fa2
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-01 22:32:30 +0700

    chore: add `/gift` as an actual api route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,18 @@
 import { Router } from "express"
 import authRouter from "@/router/authRouter"
 import codeRouter from "@/router/code"
+import giftRouter from "@/router/code"
 import systemRouter from "@/router/system"
 import userRouter from "@/router/userRouter"
 
 export default function routerManager() {
-    const router = Router()
+	const router = Router();
 
-    router.use("/auth", authRouter())
-    router.use("/code", codeRouter())
-    router.use("/system", systemRouter())
-    router.use("/user", userRouter())
+    router.use("/auth", authRouter());
+    router.use("/code", codeRouter());
+    router.use("/gift",giftRouter());
+    router.use("/system", systemRouter());
+    router.use("/user", userRouter());
 
-    return router
+	return router;
 }
