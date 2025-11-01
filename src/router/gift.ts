d5a06e953be9a1ba9dd41c1e17616fa9e13e44de
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-01 22:31:17 +0700

    feat: add `/gift/send` route

---
+++ src/router/gift.ts
@@ -0,0 +1,20 @@
+import { GiftController } from "@/controller/gift/giftController";
+import { authMiddleware } from "@/middleware/authMiddleware";
+import { GiftRepository } from "@/repository/gift/giftRepository";
+import { GiftUsecase } from "@/usecase/gift/giftUsecase";
+import { Router } from "express";
+
+export default function giftRouter() {
+	const router = Router();
+	const giftRepository = new GiftRepository();
+	const giftUsecase = new GiftUsecase(giftRepository);
+	const giftController = new GiftController(giftUsecase);
+
+	router.put(
+		"/send",
+		authMiddleware,
+		giftController.sendGift.bind(giftController)
+	);
+
+	return router;
+}
