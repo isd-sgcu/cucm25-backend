ab02722033de6675f9a79750f831a981066cf0bf
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-01 22:32:16 +0700

    fix: add `wallets` to mock user

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,6 +1,7 @@
 import { EducationLevel, RoleType } from "@prisma/client"
 
 export interface ParsedUser {
+<<<<<<< HEAD
     id: string
     studentId: string
     username: string
@@ -10,6 +11,22 @@ export interface ParsedUser {
     role: RoleType
     educationLevel: EducationLevel
     school: string
+=======
+	id: string;
+	studentId: string;
+	username: string;
+	nickname: string;
+	firstname: string;
+	lastname: string;
+	role: RoleType;
+	educationLevel: EducationLevel;
+	school: string;
+	wallets: {
+		coin_balance: number;
+		current_level: number;
+		gift_sends_remaining: number;
+	};
+>>>>>>> 4ea5318 (fix: add `wallets` to mock user)
 }
 
 export type OnboardingAnswers = Array<{
