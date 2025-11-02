4133eda30b53c82f2e630c88bbd15d6e4b745fd1
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-02 18:37:27 +0700

    feat: gift quota "resetting"

--- src/usecase/gift/giftUsecase.ts
+++ src/usecase/gift/giftUsecase.ts
@@ -2,6 +2,9 @@ import { GiftRepository } from "@/repository/gift/giftRepository";
 import { UserRepository } from "@/repository/mock/userRepository";
 import { ParsedUser } from "@/types/user";
 
+const SENDING_QUOTA = 7;
+const RESET_TIME = 1000 * 60 * 60;
+
 async function checkRecipientExistence(id: string | null) {
 	if (!id) {
 		console.warn("Recipient doesn't actually exist.");
@@ -38,6 +41,24 @@ function checkSenderBalance(sender: ParsedUser, amount: number) {
 	return true;
 }
 
+function checkSenderResetTime(sender: ParsedUser) {
+	const resetTime = new Date(sender.wallets.last_gift_sent_at);
+
+	if (
+		resetTime.getMilliseconds() !== 0 ||
+		resetTime.getSeconds() !== 0 ||
+		resetTime.getMinutes() !== 0
+	) {
+		resetTime.setHours(resetTime.getHours() + 1);
+		resetTime.setMinutes(0);
+		resetTime.setSeconds(0);
+		resetTime.setMilliseconds(0);
+	}
+
+	const now = new Date().getTime();
+	return now >= resetTime.getTime();
+}
+
 export class GiftUsecase {
 	private giftRepository: GiftRepository;
 
@@ -52,6 +73,25 @@ export class GiftUsecase {
 		amount: number
 	): Promise<{ statusCode: number; message: string }> {
 		let ok = true;
+
+		const userRepository = new UserRepository();
+
+		if (checkSenderResetTime(sender)) {
+			const remaining = sender.wallets.gift_sends_remaining;
+
+			// in the database
+			await userRepository.addSendingQuota(
+				sender.id,
+				SENDING_QUOTA - remaining
+			);
+
+			// in the current object
+			sender.wallets.gift_sends_remaining += SENDING_QUOTA - remaining;
+
+			console.log(
+				`Reset ${sender.id}'s sending quota to ${SENDING_QUOTA}, as it has been more than ${RESET_TIME} milliseconds after the last gift sending.`
+			);
+		}
 
 		if (!checkRecipientExistence(recipientId)) {
 			ok = false;
@@ -76,17 +116,18 @@ export class GiftUsecase {
 		 * Actually send the gift:
 		 * - Deduct `amount` currency from `sender`'s wallet
 		 * - Deduct 1 from the sender's quota
+		 * - Set the timestamp of the last gift send to this time.
 		 * - Add `amount` currency to `recipient`'s wallet.
 		 * - TODO: Properly log the transaction (currently it's just a `console.log`) (possibly in `giftRepository.ts`?)
 		 */
 
-		const userRepository = new UserRepository();
-
+		const timestamp = new Date();
 		await userRepository.addCoinBalance(sender.id, amount * -1);
 		await userRepository.addSendingQuota(sender.id, -1);
+		await userRepository.setLastSendTime(sender.id, timestamp);
 		await userRepository.addCoinBalance(recipientId, amount);
 		console.log(
-			`${sender.id} successfully sent ${amount} money to ${recipientId} at ${new Date().toISOString()}.`
+			`${sender.id} successfully sent ${amount} money to ${recipientId} at ${timestamp.toISOString()}.`
 		);
 
 		return {
