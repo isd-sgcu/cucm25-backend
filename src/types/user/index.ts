a72bf570714e0e328336cb9e94e7c88c0469fa5d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-07 15:55:22 +0700

    fix: add `wallets` field to `ParsedUser`

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,7 +1,6 @@
 import { EducationLevel, RoleType } from "@prisma/client"
 
 export interface ParsedUser {
-<<<<<<< HEAD
     id: string
     studentId: string
     username: string
@@ -11,23 +10,6 @@ export interface ParsedUser {
     role: RoleType
     educationLevel: EducationLevel
     school: string
-=======
-	id: string;
-	studentId: string;
-	username: string;
-	nickname: string;
-	firstname: string;
-	lastname: string;
-	role: RoleType;
-	educationLevel: EducationLevel;
-	school: string;
-	wallets: {
-		coin_balance: number;
-		current_level: number;
-		gift_sends_remaining: number;
-		last_gift_sent_at: Date;
-	};
->>>>>>> 4ea5318 (fix: add `wallets` to mock user)
 }
 
 export type OnboardingAnswers = Array<{
