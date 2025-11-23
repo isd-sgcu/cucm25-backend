7b0261ded50e917008dd172ab3d6010f692c1b94
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-23 17:29:32 +0700

    docs: clarified `current_level`'s purpose

--- src/repository/user/userRepository.ts
+++ src/repository/user/userRepository.ts
@@ -97,6 +97,7 @@ export class UserRepository {
                 },
             })
 
+<<<<<<< HEAD
             await tx.user.update({
                 where: {
                     id: id,
@@ -108,4 +109,72 @@ export class UserRepository {
             })
         })
     }
+=======
+	/**
+	 * Adds to the total coin amount (which is named `current_level` since 1 `level` is 1 `coin`) to the user with `id`'s wallet
+	 * `coin`s can be reduced, but `current_level` can't, so `current_level` acts as a total coin counter.
+	 * @param {string} id The target user's id.
+	 * @param {number} amount The amount to add. Negative numbers are allowed but should not be used unless for exceptional cases.
+	 */
+	async addTotalCoinAmount(id: string, amount: number) {
+		await prisma.user.update({
+			where: { id: id },
+			data: {
+				wallets: {
+					update: {
+						where: {
+							user_id: id,
+						},
+						data: {
+							current_level: { increment: amount },
+						},
+					},
+				},
+			},
+		});
+	}
+
+	/**
+	 * Adds sending quota to user with `id`'s wallet.
+	 * @param {string} id The target user's id.
+	 * @param {number} amount The amount to add. Negative numbers (to represent subtracting) are allowed as well.
+	 */
+	async addSendingQuota(id: string, amount: number) {
+		await prisma.user.update({
+			where: { id: id },
+			data: {
+				wallets: {
+					update: {
+						where: {
+							user_id: id,
+						},
+						data: { gift_sends_remaining: { increment: amount } },
+					},
+				},
+			},
+		});
+	}
+
+	/**
+	 * Sets the last send time for a user's wallet.
+	 * Used for enforcing the hourly limit of gift sending.
+	 * @param {string} id The target user's id.
+	 * @param {Date} timestamp The timestamp to set as the last send time.
+	 */
+	async setLastSendTime(id: string, timestamp: Date) {
+		await prisma.user.update({
+			where: { id: id },
+			data: {
+				wallets: {
+					update: {
+						where: {
+							user_id: id,
+						},
+						data: { last_gift_sent_at: timestamp },
+					},
+				},
+			},
+		});
+	}
+>>>>>>> 1a8321f (docs: clarified `current_level`'s purpose)
 }
