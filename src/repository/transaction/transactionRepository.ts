372c3f7ee1f2ede91673daee18d0a314155d7f33
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-08 05:57:49 +0700

    fix: update `TransactionRepository` to match new schema

--- src/repository/transaction/transactionRepository.ts
+++ src/repository/transaction/transactionRepository.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma";
 import { ParsedUser } from "@/types/user";
-import { TransactionType } from "@prisma/client";
 
 export class TransactionRepository {
 	// 1 exp is 1 current_level = 1 coin (they're the same)
@@ -14,9 +13,8 @@ export class TransactionRepository {
 				data: {
 					sender_user_id: sender.id,
 					recipient_user_id: recipient.id,
-					type: TransactionType.GIFT,
+					type: "GIFT",
 					coin_amount: amount,
-					exp_amount: amount,
 				},
 			});
 		});
