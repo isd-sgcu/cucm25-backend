c9b94afa56eed18942d76f079c0a14750d13ecbd
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-08 16:23:43 +0700

    fix: modify transactionRepository.ts so typescript doesnt complain

--- src/repository/transaction/transactionRepository.ts
+++ src/repository/transaction/transactionRepository.ts
@@ -1,13 +1,21 @@
 import { prisma } from "@/lib/prisma";
+import { AppError } from "@/types/error/AppError";
 import { ParsedUser } from "@/types/user";
+import { receiveMessageOnPort } from "worker_threads";
 
 export class TransactionRepository {
 	// 1 exp is 1 current_level = 1 coin (they're the same)
 	async create(
-		sender: ParsedUser,
-		recipient: ParsedUser,
+		sender: ParsedUser | null,
+		recipient: ParsedUser | null,
 		amount: number
 	): Promise<void> {
+		// to be fair this shouldn't even go in the branch
+		// but its here so that the typescript compiler doesn't complain
+		if (!sender || !recipient) {
+			throw new AppError("Missing data on transaction record.", 500);
+		}
+
 		await prisma.transaction.create({
 			data: {
 				sender_user_id: sender.id,
